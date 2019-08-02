c6a6f1318ee38de3810e41c992b038a003a41e08
Bailey Guare <eguare20@st.catherines.org>
2019-08-02 09:49:47 -0400

    adsfa

--- search.js
+++ search.js
@@ -23,6 +23,7 @@ function myFunction () {
   if (value == "transportation") {
   window.open("cars2.html");
   }
+
   if (value == "travel") {
   window.open("cars2.html");
   }
@@ -50,6 +51,7 @@ function myFunction () {
   if (value == "bedroom") {
   window.open("house.html");
   }
+
   if (value == "food") {
   window.open("food.html");
   }
@@ -71,24 +73,106 @@ function myFunction () {
   if (value == "meal") {
   window.open("food.html");
   }
+  if (value == "vegetables") {
+  window.open("food.html");
+  }
+  if (value == "fruit") {
+  window.open("food.html");
+  }
+  if (value == "cook") {
+  window.open("food.html");
+  }
+  if (value == "cooking") {
+  window.open("food.html");
+  }
+  if (value == "coupons") {
+  window.open("food.html");
+  }
+  if (value == "farmer's market") {
+  window.open("food.html");
+  }
 
   if (value == "general") {
+  window.open("generalSavings.html");
+  }
+  if (value == "budget") {
+  window.open("generalSavings.html");
+  }
+  if (value == "overall") {
+  window.open("generalSavings.html");
+  }
+  if (value == "credit") {
   window.open("generalSavings.html");
   }
 
   if (value == "reduce") {
   window.open("reduce.html");
   }
+  if (value == "less") {
+  window.open("reduce.html");
+  }
+  if (value == "plan") {
+  window.open("reduce.html");
+  }
+  if (value == "plan") {
+  window.open("reduce.html");
+  }
+  if (value == "decrease") {
+  window.open("reduce.html");
+  }
+  if (value == "amount") {
+  window.open("reduce.html");
+  }
+  if (value == "donate") {
+  window.open("reduce.html");
+  }
 
   if (value == "reuse") {
+  window.open("Reuse.html");
+  }
+  if (value == "used") {
+  window.open("Reuse.html");
+  }
+  if (value == "compost") {
+  window.open("Reuse.html");
+  }
+  if (value == "donation") {
+  window.open("Reuse.html");
+  }
+  if (value == "reusable") {
   window.open("Reuse.html");
   }
 
   if (value == "recycle") {
   window.open("recycle.html");
   }
+  if (value == "trash") {
+  window.open("recycle.html");
+  }
+  if (value == "recyclable") {
+  window.open("recycle.html");
+  }
 
   if (value == "climate change") {
   window.open("climate_change.html");
+  }
+  if (value == "global warming") {
+  window.open("climate_change.html");
+  }
+  if (value == "environment")
+  window.open("climate_change.html");
+  }
+  if (value == "climate") {
+  window.open("climate_change.html");
+  }
+  if (value == "weather") {
+  window.open("climate_change.html");
+  }
+  if (value == "animals") {
+  window.open("climate_change.html");
+  }
+
+  else {
+    alert ("not a valid search")
   }
 }
