91b818cb8b4f966f977d54884d8d063f1d96b6fe
Bailey Guare <eguare20@st.catherines.org>
2019-08-02 09:28:52 -0400

    adf

--- search.js
+++ search.js
@@ -5,12 +5,70 @@ function myFunction () {
   if (value == "cars") {
   window.open("cars2.html");
   }
-
+  if (value == "gas") {
+  window.open("cars2.html");
+  }
+  if (value == "tank") {
+  window.open("cars2.html");
+  }
+  if (value == "drive") {
+  window.open("cars2.html");
+  }
+  if (value == "driving") {
+  window.open("cars2.html");
+  }
+  if (value == "driver") {
+  window.open("cars2.html");
+  }
+  if (value == "transportation") {
+  window.open("cars2.html");
+  }
+  if (value == "travel") {
+  window.open("cars2.html");
+  }
   if (value == "house") {
   window.open("house.html");
   }
-
+  if (value == "home") {
+  window.open("house.html");
+  }
+  if (value == "room") {
+  window.open("house.html");
+  }
+  if (value == "heating") {
+  window.open("house.html");
+  }
+  if (value == "bathroom") {
+  window.open("house.html");
+  }
+  if (value == "bed") {
+  window.open("house.html");
+  }
+  if (value == "bath") {
+  window.open("house.html");
+  }
+  if (value == "bedroom") {
+  window.open("house.html");
+  }
   if (value == "food") {
+  window.open("food.html");
+  }
+  if (value == "meals") {
+  window.open("food.html");
+  }
+  if (value == "prouce") {
+  window.open("food.html");
+  }
+  if (value == "grocery") {
+  window.open("food.html");
+  }
+  if (value == "store") {
+  window.open("food.html");
+  }
+  if (value == "kitchen") {
+  window.open("food.html");
+  }
+  if (value == "meal") {
   window.open("food.html");
   }
 
