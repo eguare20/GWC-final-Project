11cfde297f51481a8e62fe4611f2a268a007be1a
Bailey Guare <eguare20@st.catherines.org>
2019-08-02 11:18:34 -0400

    asdf

--- search.js
+++ search.js
@@ -1,184 +1,79 @@
+function alertFunction() {
+  alert("Hello! I am an alert box!");
+}
+
 function myFunction () {
   var search_box = document.getElementById ("search_box");
   var value = search_box.value;
   console.log ("value is "+value);
-  if (value == "cars") {
-  window.open("cars2.html");
-  }
-  if (value == "gas") {
-  window.open("cars2.html");
-  }
-  if (value == "tank") {
-  window.open("cars2.html");
-  }
-  if (value == "drive") {
-  window.open("cars2.html");
-  }
-  if (value == "driving") {
-  window.open("cars2.html");
-  }
-  if (value == "driver") {
-  window.open("cars2.html");
-  }
-  if (value == "transportation") {
-  window.open("cars2.html");
-  }
-  if (value == "travel") {
-  window.open("cars2.html");
+  var isValid = false;
+  var cars = ["cars", "gas", "tank", "drive", "driving", "driver", "transportation", "travel"];
+  var house = ["house", "home", "room", "heating", "bathroom", "bed", "bedroom", "bathroom"];
+  var food = ["food", "meals", "produce", "grocery", "store", "kitchen", "meal", "vegetables", "fruit", "cook", "cooking", "coupons"];
+  var general = ["general savings", "general","budget", "overall", "credit"];
+  var reduce = ["reduce", "less", "plan", "decrease", "amount", "donate"];
+  var reuse = ["reuse", "used", "compost", "donation", "reusable"];
+  var recycle = ["recycle", "trash", "recyclable"]
+  var climate_change = ["climate change", "climate", "global warming", "environment", "weather", "animals", "wildlife"];
+
+  for (var i = 0; i < cars.length; i++){
+    if (value == cars[i]){
+      isValid = true;
+      window.open ("cars2.html");
+    }
   }
 
-
-
-  if (value == "house") {
-  window.open("house.html");
-  }
-  if (value == "home") {
-  window.open("house.html");
-  }
-  if (value == "room") {
-  window.open("house.html");
-  }
-  if (value == "heating") {
-  window.open("house.html");
-  }
-  if (value == "bathroom") {
-  window.open("house.html");
-  }
-  if (value == "bed") {
-  window.open("house.html");
-  }
-  if (value == "bath") {
-  window.open("house.html");
-  }
-  if (value == "bedroom") {
-  window.open("house.html");
+  for (var i = 0; i < house.length; i++){
+    if (value == house[i]) {
+      isValid = true;
+      window.open("house.html");
+    }
   }
 
-
-  if (value == "food") {
-  window.open("food.html");
-  }
-  if (value == "meals") {
-  window.open("food.html");
-  }
-  if (value == "prouce") {
-  window.open("food.html");
-  }
-  if (value == "grocery") {
-  window.open("food.html");
-  }
-  if (value == "store") {
-  window.open("food.html");
-  }
-  if (value == "kitchen") {
-  window.open("food.html");
-  }
-  if (value == "meal") {
-  window.open("food.html");
-  }
-  if (value == "vegetables") {
-  window.open("food.html");
-  }
-  if (value == "fruit") {
-  window.open("food.html");
-  }
-  if (value == "cook") {
-  window.open("food.html");
-  }
-  if (value == "cooking") {
-  window.open("food.html");
-  }
-  if (value == "coupons") {
-  window.open("food.html");
-  }
-  if (value == "farmer's market") {
-  window.open("food.html");
+  for (var i = 0; i < food.length; i++){
+    if (value == food[i]) {
+      isValid = true;
+      window.open("food.html");
+    }
   }
 
-
-  if (value == "general") {
-  window.open("generalSavings.html");
-  }
-  if (value == "budget") {
-  window.open("generalSavings.html");
-  }
-  if (value == "overall") {
-  window.open("generalSavings.html");
-  }
-  if (value == "credit") {
-  window.open("generalSavings.html");
+  for (var i = 0; i < general.length; i++){
+    if (value == general[i]) {
+      isValid = true;
+      window.open("generalSavings.html");
+    }
   }
 
-
-  if (value == "reduce") {
-  window.open("reduce.html");
-  }
-  if (value == "less") {
-  window.open("reduce.html");
-  }
-  if (value == "plan") {
-  window.open("reduce.html");
-  }
-  if (value == "plan") {
-  window.open("reduce.html");
-  }
-  if (value == "decrease") {
-  window.open("reduce.html");
-  }
-  if (value == "amount") {
-  window.open("reduce.html");
-  }
-  if (value == "donate") {
-  window.open("reduce.html");
+  for (var i = 0; i < reduce.length; i++){
+    if (value == reduce[i]) {
+      isValid = true;
+      window.open("reduce.html");
+    }
   }
 
-
-  if (value == "reuse") {
-  window.open("Reuse.html");
-  }
-  if (value == "used") {
-  window.open("Reuse.html");
-  }
-  if (value == "compost") {
-  window.open("Reuse.html");
-  }
-  if (value == "donation") {
-  window.open("Reuse.html");
-  }
-  if (value == "reusable") {
-  window.open("Reuse.html");
+  for (var i = 0; i < reuse.length; i++){
+    if (value == reuse[i]) {
+      isValid = true;
+      window.open("Reuse.html");
+    }
   }
 
-
-  if (value == "recycle") {
-  window.open("recycle.html");
-  }
-  if (value == "trash") {
-  window.open("recycle.html");
-  }
-  if (value == "recyclable") {
-  window.open("recycle.html");
+  for (var i = 0; i < recycle.length; i++){
+    if (value == recycle[i]) {
+      isValid = true;
+      window.open("recycle.html");
+    }
   }
 
-
-  if (value == "climate change") {
-  window.open("climate_change.html");
-  }
-  if (value == "global warming") {
-  window.open("climate_change.html");
-  }
-  if (value == "environment")
-  window.open("climate_change.html");
-  }
-  if (value == "climate") {
-  window.open("climate_change.html");
-  }
-  if (value == "weather") {
-  window.open("climate_change.html");
-  }
-  if (value == "animals") {
-  window.open("climate_change.html");
+  for (var i = 0; i < climate_change.length; i++){
+    if (value == climate_change[i]) {
+      isValid = true;
+      window.open("climate_change.html");
+    }
   }
 
+  if (isValid = false) {
+     alertFunction;
+  }
 
 }
