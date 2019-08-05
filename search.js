b6ffe25a6d89ca2835f380ecc5929592ceacc1d7
Bailey Guare <eguare20@st.catherines.org>
2019-08-05 14:12:12 -0400

    Asdfa

--- search.js
+++ search.js
@@ -2,6 +2,7 @@
 function myFunction () {
   var search_box = document.getElementById ("search_box");
   var value = search_box.value;
+  console.log(value.toLowerCase())
   console.log ("value is "+value);
   var isValid = false;
   var cars = ["cars","car", "gas", "tank", "drive", "driving", "driver", "transportation", "travel", "vroom", "beep"];
@@ -10,67 +11,67 @@ function myFunction () {
   var general = ["general savings", "general","budget", "overall", "credit"];
   var reduce = ["reduce", "less", "plan", "decrease", "amount", "donate"];
   var reuse = ["reuse", "used", "compost", "donation", "reusable"];
-  var recycle = ["recycle", "trash", "recyclable"];
+  var recycle = ["recycle", "trash", "recyclable", "reprocess"];
   var climate_change = ["climate change", "climate", "global warming", "environment", "weather", "animals", "wildlife"];
-  var sources = ["sources", "source", "info"];
+  var sources = ["sources", "source", "info", "information"];
 
   for (var i = 0; i < cars.length; i++){
-    if (value == cars[i]){
+    if (value.toLowerCase() === cars[i]){
       isValid = true;
       window.open ("cars2.html");
     }
   }
 
   for (var i = 0; i < house.length; i++){
-    if (value == house[i]) {
+    if (value.toLowerCase() === house[i]) {
       isValid = true;
       window.open("house.html");
     }
   }
 
   for (var i = 0; i < food.length; i++){
-    if (value == food[i]) {
+    if (value.toLowerCase() === food[i]) {
       isValid = true;
       window.open("food.html");
     }
   }
 
   for (var i = 0; i < general.length; i++){
-    if (value == general[i]) {
+    if (value.toLowerCase() === general[i]) {
       isValid = true;
       window.open("generalSavings.html");
     }
   }
 
   for (var i = 0; i < reduce.length; i++){
-    if (value == reduce[i]) {
+    if (value.toLowerCase() === reduce[i]) {
       isValid = true;
       window.open("reduce.html");
     }
   }
 
   for (var i = 0; i < reuse.length; i++){
-    if (value == reuse[i]) {
+    if (value.toLowerCase() === reuse[i]) {
       isValid = true;
       window.open("Reuse.html");
     }
   }
 
   for (var i = 0; i < recycle.length; i++){
-    if (value == recycle[i]) {
+    if (value.toLowerCase() === recycle[i]) {
       isValid = true;
       window.open("recycle.html");
     }
   }
 
   for (var i = 0; i < climate_change.length; i++){
-    if (value == climate_change[i]) {
+    if (value.toLowerCase() === climate_change[i]) {
       isValid = true;
       window.open("climate_change.html");
     }
   }
   for (var i = 0; i < sources.length; i++){
-    if (value == sources[i]){
+    if (value.toLowerCase() === sources[i]){
       isValid = true;
       window.open ("sources.html");
     }
